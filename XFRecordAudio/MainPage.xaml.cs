5ce4ac9e8bc646b1929da9f7417450c700c58b80
Gerald Versluis <gerald@verslu.is>
2020-12-22 14:07:38 +0100

    Implement AudioRecorder

--- XFRecordAudio/MainPage.xaml.cs
+++ XFRecordAudio/MainPage.xaml.cs
@@ -5,14 +5,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Plugin.AudioRecorder;
 
 namespace XFRecordAudio
 {
     public partial class MainPage : ContentPage
     {
+        private readonly AudioRecorderService audioRecorderService = new AudioRecorderService();
+
+        private readonly AudioPlayer audioPlayer = new AudioPlayer();
+
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            if (audioRecorderService.IsRecording)
+            {
+                audioRecorderService.StopRecording();
+
+                audioPlayer.Play(audioRecorderService.GetAudioFilePath());
+            }
+            else
+            {
+                audioRecorderService.StartRecording();
+            }
         }
     }
 }
